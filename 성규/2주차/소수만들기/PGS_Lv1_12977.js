97ad8b4e0e484f75bceb91302cc9300245c874ba
pangkyu <baesense@naver.com>
2022-10-24 16:05:08 +0900

    [PGS] 소수만들기 / Lv1 / 20분
    https://school.programmers.co.kr/learn/courses/30/lessons/12977

---
+++ 성규/2주차/소수만들기/PGS_Lv1_12977.js
@@ -0,0 +1,39 @@
+/**
+ * 소수만들기 
+ * 문제 설명
+주어진 숫자 중 3개의 수를 더했을 때 소수가 되는 경우의 개수를 구하려고 합니다. 숫자들이 들어있는 배열 nums가 매개변수로 주어질 때, nums에 있는 숫자들 중 서로 다른 3개를 골라 더했을 때 소수가 되는 경우의 개수를 return 하도록 solution 함수를 완성해주세요.
+
+제한사항
+nums에 들어있는 숫자의 개수는 3개 이상 50개 이하입니다.
+nums의 각 원소는 1 이상 1,000 이하의 자연수이며, 중복된 숫자가 들어있지 않습니다.
+ * 
+
+입출력 예 설명
+입출력 예 #1
+[1,2,4]를 이용해서 7을 만들 수 있습니다.
+
+ */
+
+function solution(nums) {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        const sum = nums[i] + nums[j] + nums[k];
+        if (isPrime(sum)) {
+          count += 1;
+        }
+      }
+    }
+  }
+
+  return count;
+}
+function isPrime(num) {
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
