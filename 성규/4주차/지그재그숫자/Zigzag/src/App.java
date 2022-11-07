d6abb0f46ab3319c1b7e97935166f5c63811cc50
pangkyu <baesense@naver.com>
2022-11-07 12:54:01 +0900

    [SWEA] 지그재그숫자/ D2 / 10분
    https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=2&contestProbId=AV5PxmBqAe8DFAUq&categoryId=AV5PxmBqAe8DFAUq&categoryType=CODE&problemTitle=&orderBy=PASS_RATE&selectCodeLang=JAVA&select-1=2&pageSize=30&pageIndex=1&&&&&&&&&&

---
+++ 성규/4주차/지그재그숫자/Zigzag/src/App.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ * 1부터 N까지의 숫자에서 홀수는 더하고 짝수는 뺐을 때 최종 누적된 값을 구해보자.
+
+
+[예제 풀이]
+
+N이 5일 경우,
+
+1 – 2 + 3 – 4 + 5 = 3
+
+N이 6일 경우,
+
+1 – 2 + 3 – 4 + 5 – 6 = -3
+
+
+[제약사항]
+
+N은 1 이상 10 이하의 정수이다. (1 ≤ N ≤ 10)
+
+
+[입력]
+
+가장 첫 줄에는 테스트 케이스의 개수 T가 주어지고, 그 아래로 각 테스트 케이스가 주어진다.
+
+각 테스트 케이스에는 N이 주어진다.
+
+
+[출력]
+
+각 줄은 '#t'로 시작하고, 공백을 한 칸 둔 다음 누적된 값을 출력한다.
+
+(t는 테스트 케이스의 번호를 의미하며 1부터 시작한다.)
+ */
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i = 1; i <=T; i++){
+            int N = sc.nextInt();
+            int result = 0; 
+            for(int j = 1; j <=N; j++){
+                if(j%2 ==0){
+                    result -= j;
+                }else{
+                    result +=j;
+                }
+        
+            }
+            System.out.printf("#%d %d\n", i, result);
+        }
+    }
+}
+
+/**
+ * t값을 그냥 순서대로 나열하는건줄알았는데 t값도 인풋으로 받는거였다 
+ * 입-출력값 + 문제 잘읽기..
+ */
