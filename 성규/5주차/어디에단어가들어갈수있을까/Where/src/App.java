2152a343a0a92dcb2c48b036cef62fcf796800a9
pangkyu <baesense@naver.com>
2023-03-12 13:55:54 +0900

    .

---
+++ 성규/5주차/어디에단어가들어갈수있을까/Where/src/App.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i = 1; i <= T; i++){
+            int[][] puzzle; 
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+        
+            for(int j = 0; j < N; j++ ){
+
+                for(int l = 0; l < N; l++){
+                    
+                }
+            }
+
+            System.out.println("#" + i + " ");
+        }
+    }
+}
