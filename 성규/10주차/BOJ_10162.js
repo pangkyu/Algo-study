acc6ce6f389a5448c1da9203bd06159ccf456de7
pangkyu <baesense@naver.com>
2023-04-22 16:50:22 +0900

    [BOJ] 전자레인지 / 15m
    https://www.acmicpc.net/submit/10162/59652165

---
+++ 성규/10주차/BOJ_10162.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+
+const A = 300;
+const B = 60;
+const C = 10;
+
+let answer = "";
+let temp = input;
+
+answer += `${parseInt(temp / A)} `;
+temp = temp % A;
+answer += `${parseInt(temp / B)} `;
+temp = temp % B;
+answer += `${parseInt(temp / C)} `;
+temp = temp % C;
+
+answer = temp === 0 ? answer : -1;
+console.log(answer);
